96e883325854052d30156e5faee8807b23f4d290
skid06 <valencia.mel.06@gmail.com>
2020-04-06 12:45:58 +0800

    Added UserController for fetching new Users. Also modified the user's table for additional fields for admin and user approval columns.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
+Route::post('/login', function( ){
+    $email = \Request::get('email');
+    $password = \Request::get('password');
+    
+
+    if (\Auth::attempt([
+        'email' => $email,
+        'password' => $password
+    ])) {
+        return response()->json('', 204 );
+    }else{
+        return response()->json([
+            'error' => 'invalid_credentials'
+        ], 403);
+    }
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
