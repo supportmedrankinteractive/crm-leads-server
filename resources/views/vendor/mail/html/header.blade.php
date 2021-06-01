be9e1162dd39b478cee25fe08f0a10c0c185ff02
Melchor Valencia <valencia.mel.06@gmail.com>
2021-06-01 15:25:55 +0800

    Cleaned up the unnecessary codes 3.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{url('/mrilogo.png')}}" class="logo" alt="{{config('app.name')}}" style="width: 20%;">
+<img src="http://medrankcrm.com/mrilogo.png" class="logo" alt="{{config('app.name')}}" style="width: 20%;">
 @else
-<img src="{{url('/mrilogo.png')}}" class="logo" alt="{{config('app.name')}}" style="width: 20%;">
+<img src="http://medrankcrm.com/mrilogo.png" class="logo" alt="{{config('app.name')}}" style="width: 20%;">
 <!--{{ $slot }}-->
 @endif
 </a>
