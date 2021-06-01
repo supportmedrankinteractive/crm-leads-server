eedabdd2ca032d77f62e9b6e7db900764bdff4d6
Melchor Valencia <valencia.mel.06@gmail.com>
2021-06-01 15:20:05 +0800

    Resize the image on the email template 2.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,8 @@
 @if (trim($slot) === 'Laravel')
 <img src="{{url('/mrilogo.png')}}" class="logo" alt="{{config('app.name')}}" style="width: 20%;">
 @else
-{{ $slot }}
+<img src="{{url('/mrilogo.png')}}" class="logo" alt="{{config('app.name')}}" style="width: 20%;">
+<!--{{ $slot }}-->
 @endif
 </a>
 </td>
