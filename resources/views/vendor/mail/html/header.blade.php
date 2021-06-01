e917db95816a24ebf01d0f01c370e6ea51b1b8a0
Melchor Valencia <valencia.mel.06@gmail.com>
2021-06-01 15:46:17 +0800

    Update 4.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="http://medrankcrm.com/mrilogo.png" class="logo" alt="{{config('app.name')}}" style="width: 20%;">
+<img src="http://medrankcrm.com/mrilogo.png" class="logo" alt="{{config('app.name')}}" style="width: 20%; height: 20%;">
 </a>
 </td>
 </tr>
